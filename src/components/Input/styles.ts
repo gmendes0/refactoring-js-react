05d3e4fb88d1c1aab9a4d6ba2e16e591aa048f63
mendesg9 <gmendes230@gmail.com>
2021-11-21 14:30:40 -0300

    refactor: using typescript and functional components

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
-export const Container = styled.div`
+type ContainerProps = {
+  isFocused: boolean;
+  isFilled: boolean;
+};
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
@@ -21,20 +26,20 @@ export const Container = styled.div`
     line-height: 36px;
   }
 
-  ${props =>
+  ${(props) =>
     props.isFocused &&
     css`
       color: #ff9000;
       border-color: #ff9000;
     `}
 
-  ${props =>
+  ${(props) =>
     props.isFilled &&
     css`
       color: #ff9000;
     `}
 
-  input {
+input {
     flex: 1;
     background: transparent;
     border: 0;
